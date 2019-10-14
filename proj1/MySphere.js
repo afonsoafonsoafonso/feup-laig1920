37a8ebc2f10c3ddac01ac7638b67f9c368fbe18a
João Renato Pinto <jrcp210@gmail.com>
2019-10-14 15:28:15 +0100

    Sphere textures fixed

--- proj1/MySphere.js
+++ proj1/MySphere.js
@@ -39,10 +39,9 @@ class MySphere extends CGFobject {
                     Math.sin(alpha_angle*j)
 				);
 
-                // TODO: divide by 4 instead of 2 (in X)?
 				this.texCoords.push(
-                    omega_angle*i / (2*Math.PI),
-                    1- ((alpha_angle*j + Math.PI/2) / Math.PI)
+                    j/this.stacks,
+                    i/this.slices   
 				);
 
 			}
@@ -59,52 +58,6 @@ class MySphere extends CGFobject {
 		}
 
 		this.primitiveType = this.scene.gl.TRIANGLES;
-		this.initGLBuffers();
-        /*
-        this.vertices = [];
-        this.indices = [];
-        this.normals = [];
-        this.texCoords = [];
-
-        var stackStep = Math.PI / this.stacks;
-        var sliceStep = 2*Math.PI / this.slices;
-
-        for(var i=0; i<=this.stacks; i++) {
-            for(var j=0; j<=this.slices; j++) {
-                this.vertices.push(
-                    this.radius*Math.cos(Math.PI/2-i*stackStep)*Math.cos(j*sliceStep),
-                    this.radius*Math.cos(Math.PI/2-i*stackStep)*Math.sin(j*sliceStep),
-                    this.radius*Math.sin(Math.PI/2-i*stackStep)
-                )
-                this.normals.push(
-                    Math.cos(Math.PI/2-i*stackStep)*Math.cos(j*sliceStep),
-                    Math.cos(Math.PI/2-i*stackStep)*Math.sin(j*sliceStep),
-                    Math.sin(Math.PI/2-i*stackStep)
-                )
-            }
-        }
-        //top e bottom apenas requerem triangulos para unir ao vertice do topo
-        for(var i=0; i<this.stacks; i++) {
-            for(var j=0; j<this.slices; j++) {
-                if(i!=0) {
-                    this.indices.push(
-                        i*(this.slices+1), 
-                        i*(this.slices+1)+this.stacks+1, 
-                        i*(this.slices+1)+this.stacks+2
-                    )
-                }
-                if(i!=(this.stacks-1)) {
-                    this.indices.push(
-                        i*(this.slices+1)+this.stacks+2,
-                        i*(this.slices+1)+this.stacks+1, 
-                        i*(this.slices+1)+this.stacks+2 
-                    )
-                }
-            }
-        }
-
-        this.primitiveType = this.scene.gl.TRIANGLES;
-        this.initGLBuffers();
-        */
+		this.initGLBuffers();   
     }
 }
