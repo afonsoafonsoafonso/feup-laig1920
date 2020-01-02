b0e3cf54113d1e842f3990d603c083df83a1163e
João Renato Pinto <jrcp210@gmail.com>
2020-01-02 15:07:48 +0000

    Added 3d models code

--- proj3/MyPiece.js
+++ proj3/MyPiece.js
@@ -1,11 +1,13 @@
 class MyPiece extends CGFobject {
     constructor(scene, level) {
         super(scene);
+        this.cenas = new CGFOBJModel(this.scene, 'models/ship/[.obj] (Sh3d adapted)/Arc170.obj');
         this.side = new MyCylinder(this.scene, 0.3, 0.3, 0.2, 20, 3);
         this.base = new MyCylinder(this.scene, 0.3, 0, 0, 20, 20);
         this.level = level;
         this.animation = null;
-
+        //proj3\models\ship2\Wraith Raider Starship\Wraith Raider Starship.obj
+        //proj3\models\ship\[.obj] (Sh3d adapted)\Arc170.obj
         if(level==3) {
             this.mat = new CGFappearance(scene);
             this.mat.setAmbient(255/255, 20/255, 20/255, 1.0);
@@ -27,12 +29,13 @@ class MyPiece extends CGFobject {
             this.mat.setSpecular(0/255, 156/255, 255/255, 1.0);
             this.mat.setShininess(10.0);
         }
-
+        
         this.tile = null;
     }
 
     display(){        
         this.scene.pushMatrix();
+        
         this.mat.apply();
 
         if(this.animation!=null) {
@@ -40,23 +43,29 @@ class MyPiece extends CGFobject {
         }
 
         this.scene.translate(0, 0.2, 0);
+        
+        if(!this.scene.gameOrchestrator.theme) {
+            this.scene.pushMatrix();
+            this.scene.rotate(Math.PI/2, 1, 0, 0);
+            this.side.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
-        this.side.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.translate(0,-0.2,0);
+            this.scene.rotate(Math.PI/2, 1, 0, 0);
+            this.base.display();
+            this.scene.popMatrix();
 
-        this.scene.pushMatrix();
-        this.scene.translate(0,-0.2,0);
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
-        this.base.display();
-        this.scene.popMatrix();
-
-        this.scene.pushMatrix();
-        this.scene.rotate(Math.PI/2, 1, 0, 0);
-        this.scene.rotate(Math.PI, 0, 1, 0);
-        this.base.display();
-        this.scene.popMatrix();
+            this.scene.pushMatrix();
+            this.scene.rotate(Math.PI/2, 1, 0, 0);
+            this.scene.rotate(Math.PI, 0, 1, 0);
+            this.base.display();
+            this.scene.popMatrix();
+        } else  {
+            this.scene.scale(0.001,0.001,0.001);  
+            this.cenas.display();
+        }
+        
 
         this.scene.popMatrix();
     }
